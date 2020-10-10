6b5d330180692d2c0f77464744f868809a3035c9
johannamag <69107842+johannamag@users.noreply.github.com>
2020-10-10 13:19:01 +0200

    Added Mobile Menu

---
+++ index.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
+const menuItem = document.querySelectorAll(".menu-item");
+
+menuBtn.addEventListener("click", () => {
+  toggle();
+});
+
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (menuBtn.classList.contains("open")) {
+      toggle();
+    }
+  });
+});
+
+function toggle() {
+  menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
+}
